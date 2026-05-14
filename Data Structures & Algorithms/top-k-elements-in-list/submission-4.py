598e7b562e986782e51e57b90e64efc223110bc1
Teighan Miller <teighansmiller@icloud.com>
2026-05-14 09:00:27 -0400

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hmap = defaultdict(int)
+
+        for num in nums:
+            hmap[num] += 1
+        
+        ans = []
+        for i in range(k):
+            item = max(hmap.items(), key=lambda x: x[1])
+            ans.append(item[0])
+            del hmap[item[0]]
+
+        return ans
